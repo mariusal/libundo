19f594d4724f5d49f4ae7efaca4e49d223c2ef54
fnevgeny <fnevgeny>
2012-02-14 20:38:00 +0000

    Removed duplicate CVSMake and autogen.sh; use autoreconf instead. Patch by Marius.

--- autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-libtoolize --copy --automake --force					&& \
-aclocal -I .								&& \
-autoconf								&& \
-automake --include-deps --add-missing --copy --foreign --no-force	&& \
-./configure "$@"
